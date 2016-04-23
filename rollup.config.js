864ced231d25b744ae12469d089ee0742a70b1fe
Lucas Motta <mail@lucasmotta.com>
2016-04-23 12:29:48 +0100

    Use buble instead of babel to transpile ES6
    Because it's less configuration and much faster.
    You should look into it: http://buble.surge.sh

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,24 @@
-import babel from 'rollup-plugin-babel';
+import buble from 'rollup-plugin-buble';
 import commonjs from 'rollup-plugin-commonjs';
+import json from 'rollup-plugin-json';
+import resolve from 'rollup-plugin-node-resolve';
 
 export default {
-  moduleName: 'relayout',
+  moduleName: 'sass-module-importer',
   entry: './src/index.js',
   dest: './lib/index.js',
   plugins: [
-    babel(),
-    commonjs(),
-  ],
-  external: [
-    'fs', 'path', 'es6-map', 'object-assign', 'resolve', 'resolve-bower',
+    json(),
+    buble(),
+    resolve({
+      jsnext: true,
+      main: true,
+    }),
+    commonjs({
+      extensions: ['.js', '.json'],
+    }),
   ],
   format: 'cjs',
+  external: ['es6-map', 'object-assign', 'resolve', 'resolve-bower'],
+  onwarn: () => null,
 };
